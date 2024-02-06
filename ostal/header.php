690f2f7d35c88dfe325579dc281055c19f093353
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-02-06 17:25:29 +0100

    lil changes in header and some animating

--- ostal/header.php
+++ ostal/header.php
@@ -17,32 +17,32 @@
     <?php wp_head(); ?>
   </head>
   <body>
-      <header>
-      
-        <div class="container">
-            <div class="header">
-                <a href="/" class="logolink">
-                <img class="logo" width="150" height="150" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.webp" alt="<?php _e('Les Grands Chênes', 'ostal'); ?>" />
-                <span >
-                    <h1 class="intro">
-                    <?php 
-                    $introduction = get_option('ostal_settings_field_introduction');
-                    
-                    echo $introduction;
-                    ?>
-                    </h1>
-                </span>
-                </a>
-                <ul class="nav">
-                    <li class="nav-item"><a href="/" class="nav-link"><?php _e('Accueil', 'ostal'); ?></a></li>
-                    <li class="nav-item"><a href="#descriptif" class="nav-link"><?php _e('Présentation', 'ostal'); ?></a></li>
-                    <li class="nav-item"><a href="#situation" class="nav-link"><?php _e('Localisation', 'ostal'); ?></a></li>
-                    <li class="nav-item"><a href="#fablab" class="nav-link"><?php _e('Fab&apos;Lab', 'ostal'); ?></a></li>
-                    <li class="nav-item"><a href="#ateliers" class="nav-link nav-link-magic"><?php _e('Ateliers', 'ostal'); ?></a></li>
-                    <li class="nav-item"><a href="#reservation" class="nav-link nav-link-magic"><?php _e('Reservation', 'ostal'); ?></a></li>
-                </ul>
-            </div>
-            
-            
+    <header>
+    
+
+      <div class="header">
+        <div class="logo_title">
+          <a href="/" class="logolink">
+          <img class="logo" width="150" height="150" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.webp" alt="<?php _e('Les Grands Chênes', 'ostal'); ?>" />
+          </a>
+          <ul class="nav">
+              <li class="nav-item"><a href="#descriptif" class="nav-link"><?php _e('Présentation', 'ostal'); ?></a></li>
+              <li class="nav-item"><a href="#situation" class="nav-link"><?php _e('Localisation', 'ostal'); ?></a></li>
+              <li class="nav-item"><a href="#fablab" class="nav-link"><?php _e('Fab&apos;Lab', 'ostal'); ?></a></li>
+              <li class="nav-item"><a href="#ateliers" class="nav-link nav-link-magic"><?php _e('Ateliers', 'ostal'); ?></a></li>
+              <li class="nav-item"><a href="#links" class="nav-link nav-link-magic"><?php _e('Liens utiles', 'ostal'); ?></a></li>
+          </ul>
+       
+        
         </div>
-      </header>
+        <div>
+        <p class="intro">
+        <?php 
+        $introduction = get_option('ostal_settings_field_introduction');
+        
+        echo $introduction;
+        ?>
+        </p>
+        </div>
+      </div>
+    </header>
